6399a97fcc0a909e02745f4281fbe55cdb037cac
gabriel <gabrielrsp18@gmail.com>
2022-04-03 21:06:10 -0300

    add debounce and throttling functions

---
+++ script.js
@@ -0,0 +1,56 @@
+const input = document.querySelector("input")
+const defaultText = document.getElementById("default")
+const debounceText = document.getElementById("debounce")
+const throttleText = document.getElementById("throttle")
+
+const updateDebounceText = debounce((text) => {
+  debounceText.textContent = text
+})
+
+const updateThrottleText = throttle((text) => {
+  throttleText.textContent = text
+})
+
+input.addEventListener("input", e => {
+  defaultText.textContent = e.target.value
+  updateDebounceText(e.target.value)
+  updateThrottleText(e.target.value)
+})
+
+function debounce(cb, delay= 1000) {
+  let timeout
+
+  return (...args) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      cb(...args )
+    }, delay)
+  }
+}
+
+//Throttle is usefull for resizing windows, scrolling, drag and drop
+function throttle(cb, delay = 1000) {
+  let shouldWait = false
+  let waitingArgs = true
+
+  const timeoutFunc = () => {
+    if(waitingArgs == null) {
+      shouldWait = false
+    } else {
+      cb(...waitingArgs)
+      waitingArgs = null
+      setTimeout(timeoutFunc, delay)
+    }
+  }
+  
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args
+      return   
+    }
+    cb(...args)
+    shouldWait = true 
+
+    setTimeout(timeoutFunc, delay)
+  }
+}
